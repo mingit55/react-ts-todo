2711571f70311ea2e840146dec4a3e2089c66142
mingit55 <mingit55@gmail.com>
2021-06-23 15:20:20 +0900

    fix: 기존 컴포넌트와 연결

--- src/ts/todo.tsx
+++ src/ts/todo.tsx
@@ -1,10 +1,6 @@
 import '../scss/fonts.scss';
 import '../scss/common.scss';
 import '../scss/todo.scss';
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
 import TodoEditor from './todo/TodoEditor';
 
-window.onload = () => {
-  ReactDom.render(<TodoEditor />, document.querySelector('#app'));
-};
+export default TodoEditor;
